016477beb999850836415738b92ddc21b7a46467
Andrea <actassi@gmail.com>
2023-12-18 05:42:50 -0300

    modif icon login

--- src/routes/agregarProdRouter.js
+++ src/routes/agregarProdRouter.js
@@ -1,21 +1,21 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import { findUserByEmail, findAdmin, loginUser } from '../functions/loginFunctions.js';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import ejs from 'ejs';
+// import express from 'express';
+// import bodyParser from 'body-parser';
+// import { findUserByEmail, findAdmin, loginUser } from '../functions/loginFunctions.js';
+// import path from 'path';
+// import { fileURLToPath } from 'url';
+// import ejs from 'ejs';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
-const agregarProdRouter = express.Router();
+// const agregarProdRouter = express.Router();
 
-agregarProdRouter.use(bodyParser.urlencoded({ extended: true }));
-agregarProdRouter.use(bodyParser.json());
+// agregarProdRouter.use(bodyParser.urlencoded({ extended: true }));
+// agregarProdRouter.use(bodyParser.json());
 
-agregarProdRouter.get('/agregar-prod', (req, res) => {
-    res.render('agregar-producto', { email: req.session.user.email });
-  });
+// agregarProdRouter.get('/agregar-prod', (req, res) => {
+//     res.render('agregar-producto', { email: req.session.user.email });
+//   });
   
 
-export default agregarProdRouter;
+// export default agregarProdRouter;
